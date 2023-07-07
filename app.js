b15b5afba8bc218cc9ebc67037396746568dbe18
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-07 09:53:28 +0200

    commit

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const student3 = new Student('Damiano', 'Di Lionardo', 'Front-End');
 const student4 = new Student('Isabella', 'Ottonello', 'Front-End');
 const student5 = new Student('Stefano', 'Florio', 'Front-End');
 const student6 = new Student('Bryan', 'Rojas', 'Front-End');
-const student7 = new Student('Stefania', '', 'Front-End');
-const student8 = new Student('Francesca', '', 'Front-End');
+const student7 = new Student('Stefania', 'Ghergut', 'Front-End');
+const student8 = new Student('Francesca', 'Ercolani', 'Front-End');
 const student9 = new Student('Luca', 'Verduci', 'Front-End');
-const student10 = new Student('Ares', '', 'Front-End');
+const student10 = new Student('Ares', 'Fiumicelli', 'Front-End');
 
 const studentsArray = [student1, student2, student3, student4, student5, student6, student7, student8, student9, student10];
 
