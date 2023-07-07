6ec62776731bafdfbf768e413ee69c29021146c0
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-07 12:15:52 +0200

    Update student.js

--- model/student.js
+++ model/student.js
@@ -1,21 +1,10 @@
 class Student {
-    constructor(name, surname) {
+    constructor(name, surname,) {
         this.name = name;
         this.surname = surname;
     }
 
     toString() {
         return this.name + ' ' + this.surname;
-    }
-
-    get randomizer() {
-
-        const twistStudent = [];
-
-        for (let i = 0; i < studentsArray.length; i++) {
-            const twice = s1 + s2
-            twistStudent.push(twice)
-        }
-        return twistStudent;
     }
 }
