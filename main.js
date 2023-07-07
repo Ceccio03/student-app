3b9b0386e81f399adbb0b55535d527e0edaa9673
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-07 11:54:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -11,12 +11,33 @@ const student10 = new Student('Ares', 'Fiumicelli');
 
 const class1 = new Classroom([student1, student2, student3, student4, student5, student6, student7, student8, student9, student10]);
 
-console.log(document);
-console.log(document.getElementById('main-container'));
+// console.log(document);
+// console.log(document.getElementById('main-container'));
 
-const mainContainer = document.getElementById('main-container');
-const h2Element = document.createElement('h2');
-const textNode = document.createTextNode(student1.toString());
+// const mainContainer = document.getElementById('main-container');
+// const h2Element = document.createElement('h2');
+// const textNode = document.createTextNode(student1.toString());
 
-h2Element.appendChild(textNode);
-mainContainer.appendChild(h2Element);
+// h2Element.appendChild(textNode);
+// mainContainer.appendChild(h2Element);
+
+function renderPage() {
+    const mainContainer = document.getElementById('main-container');
+
+    for (let i = 0; i < class1.studentsArray.length; i++) {
+        if (i % 2 === 0) {
+            const h3Element = document.createElement('h3');
+            const node = document.createTextNode('Coppia ' + (i / 2 + 1) + ': ');
+
+            h3Element.appendChild(node);
+            mainContainer.appendChild(h3Element);
+        }
+        const student = class1.studentsArray[i];
+        const pElement = document.createElement('p');
+        const textNode = document.createTextNode(student.toString());
+
+        pElement.appendChild(textNode);
+        mainContainer.appendChild(pElement);
+    }
+}
+renderPage();
