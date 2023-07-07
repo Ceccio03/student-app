bf33542d5e9a87e0d5577b196588871f39eb5aa1
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-07 11:10:19 +0200

    new file

---
+++ main.js
@@ -0,0 +1,14 @@
+const student1 = new Student('Cesare', 'Falzone');
+const student2 = new Student('Manuela', 'Ariotti');
+const student3 = new Student('Damiano', 'Di Lionardo');
+const student4 = new Student('Isabella', 'Ottonello');
+const student5 = new Student('Stefano', 'Florio');
+const student6 = new Student('Bryan', 'Rojas');
+const student7 = new Student('Stefania', 'Ghergut');
+const student8 = new Student('Francesca', 'Ercolani');
+const student9 = new Student('Luca', 'Verduci');
+const student10 = new Student('Ares', 'Fiumicelli');
+
+console.log(student1.toString());
+
+const class1 = new Classroom([student1, student2, student3, student4, student5, student6, student7, student8, student9, student10]);
